9b9db4dee66fb89cc3d341049d1a6bdf8f5490c7
Carlos Alvarez <alvrzcarlos@gmail.com>
2019-01-10 07:41:08 -0600

    Cambios en colores y logotipo

--- app/src/main/java/mx/com/sousystems/wbcscounter/activities/AjusteActivity.java
+++ app/src/main/java/mx/com/sousystems/wbcscounter/activities/AjusteActivity.java
@@ -2,20 +2,43 @@ package mx.com.sousystems.wbcscounter.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import mx.com.sousystems.wbcscounter.R;
+import mx.com.sousystems.wbcscounter.domain.Paciente;
 
 public class AjusteActivity extends AppCompatActivity {
+
+    private Spinner spAlertarCantidad;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ajuste);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        cargarComponentes();
+        agregarDatosSpinner();
+    }
+
+    private void cargarComponentes() {
+        spAlertarCantidad = findViewById(R.id.spAlertarCantidad);
+    }
+
+    private void agregarDatosSpinner() {
+        String[] alertQuantity = {getString(R.string.seleccione_cantidad_alertar), "100", "200", "500", "1000"};
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_spinner_item, alertQuantity);
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spAlertarCantidad.setAdapter(dataAdapter);
     }
 
     @Override
-    public boolean onSupportNavigateUp(){
+    public boolean onSupportNavigateUp() {
         finish();
         return true;
     }
